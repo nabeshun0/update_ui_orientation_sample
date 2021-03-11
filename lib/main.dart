9e85644e030c8157d6a9df2273df0ffa47666d2f
snt-nbzw <sntnbzw@gmail.com>
2021-03-11 13:19:47 +0900

    Add: implement OrientationBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,47 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final appTitle = 'Orientation Demo';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: appTitle,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: OrientationList(),
+      home: OrientationList(
+        title: appTitle,
+      ),
     );
   }
 }
 
 class OrientationList extends StatelessWidget {
+  final String title;
+
+  const OrientationList({Key key, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  'Item $index',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              );
+            }),
+          );
+        },
+      ),
+    );
   }
 }
